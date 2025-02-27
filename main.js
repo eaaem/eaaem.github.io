9ca9fe6f7218508802e43463742c0b54bf5fbc45
eaaem <iris95159@yahoo.com>
2025-02-26 22:24:12 -0800

    Added semicolon

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.getElementById("rollAll").onclick = function() { rollAll() };
 
 function rollAll() {
-  let image = document.getElementById("player1HeroImage")
+  let image = document.getElementById("player1HeroImage");
   image.src = "https://static.wikia.nocookie.net/vermintide2_gamepedia_en/images/0/0d/Portrait_kruber_mercenary.png";
 }
