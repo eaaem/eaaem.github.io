f68aa7ec6e4356ea1e604a30ce333f776a5e9e17
eaaem <iris95159@yahoo.com>
2025-02-26 22:21:58 -0800

    Trying image on click

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 document.getElementById("rollAll").onclick = function() { rollAll() };
 
 function rollAll() {
-  //document.getElementById("player1HeroImage").src = "https://static.wikia.nocookie.net/vermintide2_gamepedia_en/images/0/0d/Portrait_kruber_mercenary.png/revision/latest?cb=20180307122551";
+  let image = document.getElementById("player1HeroImage")
+  image.src = "https://static.wikia.nocookie.net/vermintide2_gamepedia_en/images/0/0d/Portrait_kruber_mercenary.png";
 }
