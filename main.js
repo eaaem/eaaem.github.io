683b42a9ebf30921135dc703433ec29b41190586
eaaem <iris95159@yahoo.com>
2025-02-26 21:38:45 -0800

    Testing image display

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-document.getElementById("testButton").onclick = function() { test() };
+document.getElementById("rollAll").onclick = function() { rollAll() };
 
-function test() {
-  document.getElementById("testButton").innerHTML = "MyTest";
+function rollAll() {
+  document.getElementById("player1HeroImage").src = "https://static.wikia.nocookie.net/vermintide2_gamepedia_en/images/0/0d/Portrait_kruber_mercenary.png/revision/latest?cb=20180307122551";
 }
